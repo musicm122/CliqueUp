ded058ea05ef4c65de25e50e1f2a5a7dba0b40b1
musicm122 <musicm122@gmail.com>
2013-06-02 10:02:57 -0400

    Big morning commit

--- Web/Scripts/main.js
+++ Web/Scripts/main.js
@@ -1,18 +1,34 @@
-﻿
+﻿\
+
 require.config({
     paths: {
         jquery: 'Scripts/jquery-1.9.1',
         bootstrap: "Scripts/bootstrap",
-        amplify:"Scripts/amplify"
+        amplify: "Scripts/amplify",
+        ko: "Scripts/knockout-2.2.0"
     }
 
 });
 
 
-require(["jquery", "bootstrap", "amplify"], function ($, bootstrap, amplify) {
+require(["jquery", "bootstrap", "amplify"], function ($, bootstrap, amplify, ko) {
     // Modules that do stuff on every page are instantiated here 
     $(document).ready(function () {
         var self = this;
+        self.term = "";
+        self.distance = 10;//in miles
+
+        $("#EventSearchBtn").click(function (e) {
+            $("#searchText").val();
+            amplify.request("CreateEvent", {
+                term: self.term,
+                location: "",
+                radius:self.distance
+            }, function (data) {
+
+                $("#searchText").html();
+            });
+        });
 
         amplify.request.define("EventSearch", "ajax", {
             url: "/Search/{term}/{location}/{radius}/",
@@ -20,7 +36,7 @@ require(["jquery", "bootstrap", "amplify"], function ($, bootstrap, amplify) {
             cache: true
         });
 
-        amplify.request.define("CreatEvent", "ajax", {
+        amplify.request.define("CreateEvent", "ajax", {
             url: "/Search/{term}/{location}/{radius}/",
             dataType: "json",
             cache: true
