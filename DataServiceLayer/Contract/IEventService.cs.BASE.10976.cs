55226b2d2643da89ac9233a7d2b4c2ee1cf0ad04
musicm122 <musicm122@gmail.com>
2013-06-01 21:50:25 -0400

    merge conflict in event service

---
+++ DataServiceLayer/Contract/IEventService.cs.BASE.10976.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CliqueUpModel.Model;
+
+namespace CliqueUpModel.Contract
+{
+    interface IEventService
+    {
+        /// <summary>
+        /// Creates an event
+        /// </summary>
+        /// <param name="title">Title of the event</param>
+        /// <param name="description">Description of the event</param>
+        /// <param name="categories">List of categories tied to this event</param>
+        /// <param name="start">Expected start time of the event</param>
+        /// <param name="end">Expected end time of the event</param>
+        /// <param name="lat">Approximate latitude of the event</param>
+        /// <param name="lon">Approximate longitude of the event</param>
+        /// <returns>Event object created.</returns>
+        Event CreateEvent(string title, string description, IEnumerable<string> categories, DateTime start, DateTime end, decimal lat, decimal lon);
+        
+        /// <summary>
+        /// Marks an event as active
+        /// </summary>
+        /// <param name="userId">The ID of the user opening the event</param>
+        /// <param name="id">The ID of the event</param>
+        /// <returns>boolean denoting whether the event was opened successfully or not</returns>
+        bool OpenEvent(Guid userId, Guid id);
+
+        /// <summary>
+        /// Marks an event as closed
+        /// </summary>
+        /// <param name="userId">The ID of the user closing the event</param>
+        /// <param name="id">The ID of the event</param>
+        /// <returns>boolean denoting whether the event was closed successfully or not</returns>
+        bool CloseEvent(Guid userId, Guid id);
+    }
+}
---
+++ DataServiceLayer/Contract/IEventService.cs.REMOTE.10976.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CliqueUpModel.Model;
+
+namespace CliqueUpModel.Contract
+{
+    interface IEventService
+    {
+        /// <summary>
+        /// Creates an event
+        /// </summary>
+        /// <param name="title">Title of the event</param>
+        /// <param name="description">Description of the event</param>
+        /// <param name="categories">List of categories tied to this event</param>
+        /// <param name="start">Expected start time of the event</param>
+        /// <param name="end">Expected end time of the event</param>
+        /// <param name="lat">Approximate latitude of the event</param>
+        /// <param name="lon">Approximate longitude of the event</param>
+        /// <returns>Event object created.</returns>
+        Event CreateEvent(string title, string description, IEnumerable<string> categories, DateTime start, DateTime end, double lat, double lon);
+        
+        /// <summary>
+        /// Marks an event as active
+        /// </summary>
+        /// <param name="userId">The ID of the user opening the event</param>
+        /// <param name="id">The ID of the event</param>
+        /// <returns>boolean denoting whether the event was opened successfully or not</returns>
+        bool OpenEvent(Guid userId, Guid id);
+
+        /// <summary>
+        /// Marks an event as closed
+        /// </summary>
+        /// <param name="userId">The ID of the user closing the event</param>
+        /// <param name="id">The ID of the event</param>
+        /// <returns>boolean denoting whether the event was closed successfully or not</returns>
+        bool CloseEvent(Guid userId, Guid id);
+
+        /// <summary>
+        /// Finds all events matching search parameters
+        /// </summary>
+        /// <param name="searchQuery">Search query that looks over title and description and categories.  Categories
+        /// are space delimited and start with # character</param>
+        /// <param name="baseLatitude"></param>
+        /// <param name="baseLongitude"></param>
+        /// <param name="searchRadiusMiles"></param>
+        /// <returns></returns>
+        IEnumerable<Event> SearchEvents(string searchQuery, double baseLatitude, double baseLongitude, int searchRadiusMiles);
+
+        EventMessage PostEventMessage(Guid userid, Guid eventId, string messageText);
+    }
+}
